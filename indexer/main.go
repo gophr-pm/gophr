b56da59d077b4a750d2417aa262efc77a902cc52
Dan Cadden <shikkic@gmail.com>
2016-04-07 23:03:39 -0400

    first pass at creating list of github packages from go doc

--- indexer/main.go
+++ indexer/main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	//"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 func main() {
-	fmt.Println("THE\nINDEXER\nRAN\nFAM.")
+	log.Println("Begining download")
+	doc, err := goquery.NewDocument("https://godoc.org/-/index")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Finished download")
+	var buffer bytes.Buffer
+
+	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
+		children := s.Children()
+
+		var (
+			githubURL   string
+			description string
+			//goDocURL    string
+		)
+
+		children.Each(func(i int, s2 *goquery.Selection) {
+			childURL, childURLexists := s.Find("a").Attr("href")
+			childDescription := s.Text()
+
+			if childURLexists == true {
+				githubURL = childURL
+			}
+
+			if len(childDescription) > 0 {
+				description = childDescription
+			}
+		})
+
+		if strings.Contains(githubURL, "github.com") {
+			//goDocUrl =
+
+			if githubURL == "/"+githubURL {
+				description = ""
+			}
+
+			buffer.WriteString("{\"url\": \"" + githubURL + "\", \"description\": \"" + description + "\"},\n")
+		}
+	})
+
+	jsonData := []byte(buffer.String())
+	_ = ioutil.WriteFile("./tmp.json", jsonData, 0644)
+	log.Println("Finished")
 }
