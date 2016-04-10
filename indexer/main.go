6582bc51a462dbdd88c31f2dba230ebe736bb9d5
Dan Cadden <shikkic@gmail.com>
2016-04-10 16:23:01 -0400

    use go routine to make requests to github, plus general clean up of indexer

--- indexer/main.go
+++ indexer/main.go
@@ -21,10 +21,6 @@ type GoPackage struct {
 	Versions       []string
 }
 
-const (
-	refsFetchURLTemplate = "https://%s.git/info/refs?service=git-upload-pack"
-)
-
 func main() {
 	log.Println("Started Download of Godoc/index")
 
@@ -33,10 +29,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("Finished Download of Godoc/index")
+	log.Println("Finished Download of Godoc/index\n")
 	log.Println("Started scraping all github packages from Godoc/index")
 
-	var goPackageList = make([]*GoPackage, 0)
 	var goPackageMap = make(map[string]*GoPackage)
 
 	// For each tr element on the page
@@ -61,7 +56,9 @@ func main() {
 
 		// Only continue if this goPackage contains a GitHub URL
 		// TODO check to make sure github.com is the prefix
-		if strings.Contains(goPackage.GitHubURL, "github.com") {
+		gitHubUrlTokens := strings.Split(goPackage.GitHubURL, "/")
+
+		if strings.Contains(goPackage.GitHubURL, "github.com") && len(gitHubUrlTokens) == 3 {
 			// Build go doc url
 			goPackage.GoDocURL = ("https://godoc.org/" + goPackage.GitHubURL)
 
@@ -70,14 +67,11 @@ func main() {
 			time := t.String()
 			goPackage.IndexTime = time
 
-			// Add goPackage to goPackageList
-			goPackageList = append(goPackageList, goPackage)
-			// Hash the goPackage.GitHubURL for lookup
 			goPackageMap[goPackage.GitHubURL] = goPackage
 		}
 	})
 
-	log.Println("Finished scraping all github packages from Godoc/index")
+	log.Println("Finished scraping ", len(goPackageMap), " github packages from Godoc/index\n")
 	log.Println("Started Download of awesome-go/README.md")
 
 	doc, err = goquery.NewDocument("https://godoc.org/-/index")
@@ -85,60 +79,83 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("Finished Download of awesome-go/README.md")
+	log.Println("Finished Download of awesome-go/README.md\n")
 	log.Println("Started scraping awesome-go/README.md")
 
+	awesomeGoCount := 0
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
 		childURL, _ := s.Attr("href")
-
+		fmt.Println(childURL)
 		goPackage, goPackageExists := goPackageMap[childURL]
 
 		if goPackageExists {
 			goPackage.AwesomeGo = true
 			goPackageMap[childURL] = goPackage
-			printGoPackage(goPackage)
+			awesomeGoCount++
 		}
 	})
 
-	log.Println("Finished scraping awesome-go/README.md")
+	log.Println("Finished scraping ", awesomeGoCount, " awesome-go/README.md\n")
+	log.Println("Started retrieving versions for go packages")
+
+	resc, errc := make(chan *GoPackage), make(chan *GoPackage)
+	var goErrorPackageMap = make(map[string]*GoPackage)
 
 	for _, goPackage := range goPackageMap {
-		refs, err := common.FetchRefs(goPackage.GitHubURL)
-		if err != nil {
-			log.Println("ERROR", goPackage.GitHubURL, " failed to return.", err)
-		}
+		go func(goPackage *GoPackage) {
+			refs, err := common.FetchRefs(goPackage.GitHubURL)
+			if err != nil {
+				log.Println("ERROR", goPackage.GitHubURL, " failed to return.", err)
+				errc <- goPackage
+				return
+			}
 
-		var versions []string
-		for _, version := range refs.Candidates {
-			versions = append(versions, version.String())
-		}
-
-		goPackage.Versions = versions
+			var versions []string
+			for _, version := range refs.Candidates {
+				versions = append(versions, version.String())
+			}
+			goPackage.Versions = versions
+			resc <- goPackage
+		}(goPackage)
 	}
 
-	log.Println("Finished Building GoPackages")
-	log.Println("Creating JSON dump")
-	createJSONDump(goPackageList)
+	// Create Errored out Map
+	length := len(goPackageMap)
+	for i := 0; i < length; i++ {
+		select {
+		case goPackage := <-resc:
+			goPackageMap[goPackage.GitHubURL] = goPackage
+		case errPackage := <-errc:
+			delete(goPackageMap, errPackage.GitHubURL)
+			goErrorPackageMap[errPackage.GitHubURL] = errPackage
+		}
+	}
+
+	log.Println("Finished Building ", len(goPackageMap), " GoPackages")
+	log.Println("Creating JSON dump of ", len(goPackageMap), " valid go packages")
+	createJSONDump(goPackageMap, "valid-go-packages")
+	log.Println("Finished creating JSON dump\n")
+
+	log.Println(len(goErrorPackageMap), " GoPackages where not found on github")
+	log.Println("Creating JSON dump of ", len(goPackageMap), " err packages")
+	createJSONDump(goErrorPackageMap, "invalid-go-packages")
+	log.Println("Finished creating JSON dump")
 }
 
 // Create a tmp JSON dump of all serialized goPackageData
-func createJSONDump(goPackageList []*GoPackage) {
+func createJSONDump(goPackageMap map[string]*GoPackage, fileName string) {
 	var buffer bytes.Buffer
 
-	for _, goPackage := range goPackageList {
-		buffer.WriteString("{\"url\": \"" + goPackage.GitHubURL + "\", \"description\": \"" + goPackage.Description + "\", \"index_time\": \"" + goPackage.IndexTime + "\" },\n")
+	for _, goPackage := range goPackageMap {
+		buffer.WriteString("{\"url\": \"" + goPackage.GitHubURL + "\", \"description\": \"" + goPackage.Description + "\", \"index_time\": \"" + goPackage.IndexTime + "\" }, \"versions\": \"" + fmt.Sprintf("%v", goPackage.Versions) + "\" \n")
 	}
 
 	t := time.Now()
-	time := t.String()
+	time := strings.Replace(t.String(), " ", "", -1)
 	jsonData := []byte(buffer.String())
-	_ = ioutil.WriteFile("./"+time+"tmp.json", jsonData, 0644)
+	_ = ioutil.WriteFile("./"+time+"-"+fileName+".json", jsonData, 0644)
 }
 
-func printGoPackage(gp *GoPackage) {
-	fmt.Println("GitHub URL = ", gp.GitHubURL)
-	fmt.Println("Description = ", gp.Description)
-	fmt.Println("Index time = ", gp.IndexTime)
-	fmt.Println("GoDocURL = ", gp.GoDocURL)
-	fmt.Println("AwesomeGo = ", gp.AwesomeGo, "\n")
+func printGoPackage(goPackage *GoPackage) {
+	fmt.Printf("%v", goPackage)
 }
