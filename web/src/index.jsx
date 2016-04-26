f58192811e7ae401ed9ea3b51e8f843ec9915613
yaw asabere <yasabere@gmail.com>
2016-04-25 22:23:26 -0400

    Added web skeleton code

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -5,6 +5,7 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
+  <h1>Gophr Skeleton code</h1>
   <Voting pair={pair} />,
   document.getElementById('app')
 );
