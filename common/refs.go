5c8721be67d87c26f327dc3b9d920c131961d558
Shikkic <Shikkic@gmail.com>
2016-08-25 14:53:11 -0400

    created CheckIfRefExists to determine if ref has already been versioned

--- common/refs.go
+++ common/refs.go
@@ -329,3 +329,38 @@ func (refsData Refs) Reserialize(versionRefName, versionRefHash string) []byte {
 
 	return buf.Bytes()
 }
+
+// CheckIfRefExists downloads and processes refs data from Github and checks
+// whether a given ref exists in the remote refs list.
+func CheckIfRefExists(author, repo string, ref string) (bool, error) {
+	githubRoot := fmt.Sprintf(
+		githubRootTemplate,
+		author,
+		repo,
+	)
+
+	res, err := httpClient.Get(fmt.Sprintf(refsFetchURLTemplate, githubRoot))
+	if err != nil {
+		return false, errors.New(errorRefsFetchNetworkFailure)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 && res.StatusCode < 500 {
+		return false, fmt.Errorf(errorRefsFetchNoSuchRepo, githubRoot)
+	} else if res.StatusCode >= 500 {
+		// FYI no reliable way to get test coverage here; this never happens
+		return false, fmt.Errorf(errorRefsFetchGithubError, res.Status)
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		// FYI no reliable way to get test coverage here; this never happens
+		return false, fmt.Errorf(errorRefsFetchGithubParseError, err)
+	}
+
+	refsString := string(data)
+	refExists := strings.Contains(refsString, ref)
+
+	return refExists, nil
+}
