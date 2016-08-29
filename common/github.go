f41d171a70e1a028fd0a21edd62b8cdb1d00ebc0
Shikkic <Shikkic@gmail.com>
2016-08-29 19:20:29 -0400

    github service can now fetch timetamp of commit SHA

--- common/github.go
+++ common/github.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/pquerna/ffjson/ffjson"
 	"github.com/skeswa/gophr/common/dtos"
 	"github.com/skeswa/gophr/common/models"
 )
@@ -188,6 +189,41 @@ func BuildNewGitHubRepoName(author string, repo string) string {
 	return fmt.Sprintf("%d%s%d%s", len(author), author, len(repo), repo)
 }
 
+func (gitHubRequestService *GitHubRequestService) FetchCommitTimestamp(
+	packageModel models.PackageModel,
+	commitSHA string,
+) (time.Time, error) {
+	APIKeyModel := gitHubRequestService.APIKeyChain.getAPIKeyModel()
+	log.Println(APIKeyModel)
+	fmt.Printf("%+v \n", APIKeyModel)
+	log.Printf("Determining APIKey %s \n", APIKeyModel.Key)
+
+	githubURL := buildGitHubCommitTimestampAPIURL(&packageModel, *APIKeyModel, commitSHA)
+	log.Printf("Fetching commit timestamp for %s \n", githubURL)
+
+	resp, err := http.Get(githubURL)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return time.Time{}, errors.New("Request error.")
+	}
+
+	if resp.StatusCode == 404 {
+		log.Println("PackageModel was not found on Github")
+		return time.Time{}, nil
+	}
+
+	APIKeyModel.incrementUsageFromResponseHeader(resp.Header)
+	APIKeyModel.print()
+
+	timestamp, err := parseGitHubCommitLookUpResponseBody(resp)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return timestamp, nil
+}
+
 // FetchCommitSHA Fetches a commitSHA closest to a given timestamp
 func (gitHubRequestService *GitHubRequestService) FetchCommitSHA(
 	packageModel models.PackageModel,
@@ -241,7 +277,7 @@ func (gitHubRequestService *GitHubRequestService) fetchCommitSHAByTimeSelector(
 	APIKeyModel.incrementUsageFromResponseHeader(resp.Header)
 	APIKeyModel.print()
 
-	commitSHA, err := parseGitHubCommitLookUpResponseBody(resp)
+	commitSHA, err := parseGitHubCommitTimestamp(resp)
 	if err != nil {
 		return "", err
 	}
@@ -269,14 +305,51 @@ func buildGitHubRepoCommitsFromTimestampAPIURL(
 	return url
 }
 
-func parseGitHubCommitLookUpResponseBody(response *http.Response) (string, error) {
+func buildGitHubCommitTimestampAPIURL(
+	packageModel *models.PackageModel,
+	APIKeyModel GitHubAPIKeyModel,
+	commitSHA string,
+) string {
+	author := *packageModel.Author
+	repo := *packageModel.Repo
+
+	url := fmt.Sprintf("%s/repos/%s/%s/commits/%s?&access_token=%s",
+		GitHubBaseAPIURL,
+		author,
+		repo,
+		commitSHA,
+		APIKeyModel.Key,
+	)
+	return url
+}
+
+func parseGitHubCommitLookUpResponseBody(response *http.Response) (time.Time, error) {
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return time.Time{}, errors.New("Failed to parse response body")
+	}
+
+	var commitLookUpDTO dtos.GitCommitLookUpDTO
+	err = ffjson.Unmarshal(body, &commitLookUpDTO)
+	if err != nil {
+		return time.Time{}, errors.New("Failed to unmarshal response body")
+	}
+
+	if commitLookUpDTO.Commit != nil && commitLookUpDTO.Commit.Committer != nil {
+		return commitLookUpDTO.Commit.Committer.Date, nil
+	}
+
+	return time.Time{}, errors.New("No commit timestamp avilable for given SHA")
+}
+
+func parseGitHubCommitTimestamp(response *http.Response) (string, error) {
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return "", errors.New("Failed to parse response body")
 	}
 
 	var commitSHAArray []dtos.GitCommitDTO
-	err = json.Unmarshal(body, &commitSHAArray)
+	err = ffjson.Unmarshal(body, &commitSHAArray)
 	if err != nil {
 		return "", errors.New("Failed to unmarshal response body")
 	}
