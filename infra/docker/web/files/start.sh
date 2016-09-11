b82f526ca9b913adba555e13debb7bd232c2c7db
Shikkic <Shikkic@gmail.com>
2016-09-11 01:39:00 -0400

    Web can now redirect to depot

--- infra/docker/web/files/start.sh
+++ infra/docker/web/files/start.sh
@@ -4,5 +4,8 @@ echo "Replacing \"{{API_ADDR}}\" in /etc/nginx/nginx.conf with $API_SVC_SERVICE_
 sed -i "s/{{API_ADDR}}/${API_SVC_SERVICE_HOST}/g;" /etc/nginx/nginx.conf
 echo "Replacing \"{{ROUTER_ADDR}}\" in /etc/nginx/nginx.conf with $ROUTER_SVC_SERVICE_HOST..."
 sed -i "s/{{ROUTER_ADDR}}/${ROUTER_SVC_SERVICE_HOST}/g;" /etc/nginx/nginx.conf
+echo "Replacing \"{{DEPOT_ADDR}}\" in /etc/nginx/nginx.conf with $DEPOT_SVC_SERVICE_HOST..."
+sed -i "s/{{DEPOT_ADDR}}/${DEPOT_SVC_SERVICE_HOST}/g;" /etc/nginx/nginx.conf
+
 echo "Starting nginx..."
 nginx -g 'daemon off;'
