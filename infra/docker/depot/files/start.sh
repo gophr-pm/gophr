22723ed98b9938ed40edc8dd48a69c4f9dd2b5b4
Shikkic <Shikkic@gmail.com>
2016-09-11 01:39:10 -0400

    modify start script of depot

--- infra/docker/depot/files/start.sh
+++ infra/docker/depot/files/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Replacing \"{{API_ADDR}}\" in /etc/nginx/conf.d/git.conf with $API_SVC_SERVICE_HOST..."
-sed -i "s/{{API_ADDR}}/${API_SVC_SERVICE_HOST}/g;" /etc/nginx/conf.d/git.conf
-echo "Replacing \"{{ROUTER_ADDR}}\" in /etc/nginx/conf.d/git.conf with $ROUTER_SVC_SERVICE_HOST..."
-sed -i "s/{{ROUTER_ADDR}}/${ROUTER_SVC_SERVICE_HOST}/g;" /etc/nginx/conf.d/git.conf
-
 echo "Starting fcgi..."
 /etc/init.d/spawn-fcgi start
 echo "Starting nginx..."
