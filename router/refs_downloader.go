04a3ba71b4d2001e2a7c221db38dae762400b176
Sandile Keswa <me@sandile.io>
2016-09-14 03:00:10 -0400

    fleshed out the refs downloader a bit

--- router/refs_downloader.go
+++ router/refs_downloader.go
@@ -8,3 +8,16 @@ type refsDownloader interface {
 	// and repo.
 	downloadRefs(author, repo string) (common.Refs, error)
 }
+
+// defaultRefsDownloader is a refs downloader that actually goes out to the
+// network in order to download refs.
+type defaultRefsDownloader struct{}
+
+// downloadRefs downloads the refs for a package matching the specified author
+// and repo.
+func (d defaultRefsDownloader) downloadRefs(
+	author string,
+	repo string) (common.Refs, error) {
+	// This type simply serves as a proxy for the actual refs package.
+	return common.FetchRefs(author, repo)
+}
