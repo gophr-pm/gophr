405528b534827f029003da418babaf65ecf74f25
Shikkic <Shikkic@gmail.com>
2016-09-15 04:06:33 -0400

    refactor unnecessary github lib code

--- common/github/helpers.go
+++ common/github/helpers.go
@@ -1,10 +1,6 @@
 package github
 
-import (
-	"fmt"
-
-	"github.com/skeswa/gophr/common/models"
-)
+import "fmt"
 
 // ParseStarCount TODO Won't need this after implementing FFJSON
 func ParseStarCount(responseBody map[string]interface{}) int {
@@ -17,27 +13,15 @@ func ParseStarCount(responseBody map[string]interface{}) int {
 }
 
 // BuildGitHubBranch creates a new ref based on a hash of the old ref
-// TODO Delete
+// TODO Delete, but router depends on it, fix it
 func BuildGitHubBranch(ref string) string {
 	repoHash := ref[:len(ref)-1]
 	return repoHash
 }
 
-// BuildRemoteURL creates a remote url for a packageModel based on it's ref
-// TODO Delete
-func BuildRemoteURL(packageModel *models.PackageModel, ref string) string {
-	repoName := BuildNewGitHubRepoName(*packageModel.Author, *packageModel.Repo)
-	remoteURL := fmt.Sprintf(gitHubRemoteOrigin, repoName)
-	return remoteURL
-}
-
 // BuildNewGitHubRepoName creates a new repo name hash uses for repo creation
 // and lookup. Eliminates collision between similar usernames and packages
-// TODO Delete
+// TODO Delete, but router depends on it, fix it
 func BuildNewGitHubRepoName(author string, repo string) string {
 	return fmt.Sprintf("%d%s%d%s", len(author), author, len(repo), repo)
-}
-
-func BuildNewGitHubRepoName2(author string, repo string, ref string) string {
-	return fmt.Sprintf("%d%s%d%s-%s", len(author), author, len(repo), repo, ref[:6])
 }
