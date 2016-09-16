446623155de09184f9e1abdd266589590baba258
Shikkic <Shikkic@gmail.com>
2016-09-16 18:32:27 -0400

    delete readme handler and create blob handler

--- api/main.go
+++ api/main.go
@@ -19,8 +19,14 @@ func main() {
 	// Register all of the routes.
 	r := mux.NewRouter()
 	r.HandleFunc("/status", StatusHandler()).Methods("GET")
-	r.HandleFunc("/readme", ReadmeHandler()).Methods("GET")
 	r.HandleFunc("/search", SearchHandler(session)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf(
+		"/blob/{%s}/{%s}/{%s}/{%s}",
+		blobHandlerURLVarAuthor,
+		blobHandlerURLVarRepo,
+		blobHandlerURLVarSHA,
+		blobHandlerURLVarPath),
+		BlobHandler()).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/{%s}/{%s}/versions", urlVarAuthor, urlVarRepo), VersionsHandler(session)).Methods("GET")
 	r.HandleFunc(fmt.Sprintf("/{%s}/{%s}/versions/latest", urlVarAuthor, urlVarRepo), LatestVersionHandler(session)).Methods("GET")
 
