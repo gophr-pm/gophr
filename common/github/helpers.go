1bdeff8cf90626c076c3fb8b27abd94869157acc
Shikkic <Shikkic@gmail.com>
2016-09-20 04:31:08 -0400

    remove dead github code

--- common/github/helpers.go
+++ common/github/helpers.go
@@ -1,7 +1,5 @@
 package github
 
-import "fmt"
-
 // ParseStarCount TODO Won't need this after implementing FFJSON
 func ParseStarCount(responseBody map[string]interface{}) int {
 	starCount := responseBody["stargazers_count"]
@@ -10,18 +8,4 @@ func ParseStarCount(responseBody map[string]interface{}) int {
 	}
 
 	return int(starCount.(float64))
-}
-
-// BuildGitHubBranch creates a new ref based on a hash of the old ref
-// TODO Delete, but router depends on it, fix it
-func BuildGitHubBranch(ref string) string {
-	repoHash := ref[:len(ref)-1]
-	return repoHash
-}
-
-// BuildNewGitHubRepoName creates a new repo name hash uses for repo creation
-// and lookup. Eliminates collision between similar usernames and packages
-// TODO Delete, but router depends on it, fix it
-func BuildNewGitHubRepoName(author string, repo string) string {
-	return fmt.Sprintf("%d%s%d%s", len(author), author, len(repo), repo)
 }
