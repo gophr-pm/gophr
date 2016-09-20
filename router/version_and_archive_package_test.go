f5fd18b46cbf85b0c646fd5b278e831cafa9d545
Shikkic <Shikkic@gmail.com>
2016-09-20 05:15:42 -0400

    version_and_archive has 100% test coverage

--- router/version_and_archive_package_test.go
+++ router/version_and_archive_package_test.go
@@ -167,4 +167,181 @@ func TestVersionAndArchivePackage(t *testing.T) {
 	err = versionAndArchivePackage(args)
 	assert.Nil(t, err)
 
+	args = packageVersionerArgs{
+		sha:    "mysha",
+		repo:   "myrepo",
+		author: "myauthor",
+		downloadPackage: func(args packageDownloaderArgs) (packageDownloadPaths, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			assert.Equal(t, "/my/cons/path", args.constructionZonePath)
+			return packageDownloadPaths{
+				archiveDirPath: "/archive/dir/path",
+			}, nil
+		},
+		constructionZonePath: "/my/cons/path",
+		versionDeps: func(args verdeps.VersionDepsArgs) error {
+			assert.Equal(t, "myauthor", args.Author)
+			assert.Equal(t, "myrepo", args.Repo)
+			assert.Equal(t, "mysha", args.SHA)
+			assert.Equal(t, "/archive/dir/path", args.Path)
+			return nil
+		},
+		attemptWorkDirDeletion: func(workDirPath string) {
+			return
+		},
+		createDepotRepo: func(author, repo, sha string) (bool, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return false, nil
+		},
+		isPackageArchived: func(args packageArchivalArgs) (bool, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return false, nil
+		},
+	}
+	err = versionAndArchivePackage(args)
+	assert.NotNil(t, err)
+
+	args = packageVersionerArgs{
+		sha:    "mysha",
+		repo:   "myrepo",
+		author: "myauthor",
+		downloadPackage: func(args packageDownloaderArgs) (packageDownloadPaths, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			assert.Equal(t, "/my/cons/path", args.constructionZonePath)
+			return packageDownloadPaths{
+				archiveDirPath: "/archive/dir/path",
+			}, nil
+		},
+		constructionZonePath: "/my/cons/path",
+		versionDeps: func(args verdeps.VersionDepsArgs) error {
+			assert.Equal(t, "myauthor", args.Author)
+			assert.Equal(t, "myrepo", args.Repo)
+			assert.Equal(t, "mysha", args.SHA)
+			assert.Equal(t, "/archive/dir/path", args.Path)
+			return nil
+		},
+		attemptWorkDirDeletion: func(workDirPath string) {
+			return
+		},
+		createDepotRepo: func(author, repo, sha string) (bool, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return true, nil
+		},
+		pushToDepot: func(args packagePusherArgs) error {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return errors.New("this is an error")
+		},
+		destroyDepotRepo: func(author, repo, sha string) error {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return errors.New("this is an error")
+		},
+	}
+	err = versionAndArchivePackage(args)
+	assert.NotNil(t, err)
+
+	args = packageVersionerArgs{
+		sha:    "mysha",
+		repo:   "myrepo",
+		author: "myauthor",
+		downloadPackage: func(args packageDownloaderArgs) (packageDownloadPaths, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			assert.Equal(t, "/my/cons/path", args.constructionZonePath)
+			return packageDownloadPaths{
+				archiveDirPath: "/archive/dir/path",
+			}, nil
+		},
+		constructionZonePath: "/my/cons/path",
+		versionDeps: func(args verdeps.VersionDepsArgs) error {
+			assert.Equal(t, "myauthor", args.Author)
+			assert.Equal(t, "myrepo", args.Repo)
+			assert.Equal(t, "mysha", args.SHA)
+			assert.Equal(t, "/archive/dir/path", args.Path)
+			return nil
+		},
+		attemptWorkDirDeletion: func(workDirPath string) {
+			return
+		},
+		createDepotRepo: func(author, repo, sha string) (bool, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return true, nil
+		},
+		pushToDepot: func(args packagePusherArgs) error {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return errors.New("this is an error")
+		},
+		destroyDepotRepo: func(author, repo, sha string) error {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return nil
+		},
+	}
+	err = versionAndArchivePackage(args)
+	assert.NotNil(t, err)
+
+	args = packageVersionerArgs{
+		sha:    "mysha",
+		repo:   "myrepo",
+		author: "myauthor",
+		downloadPackage: func(args packageDownloaderArgs) (packageDownloadPaths, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			assert.Equal(t, "/my/cons/path", args.constructionZonePath)
+			return packageDownloadPaths{
+				archiveDirPath: "/archive/dir/path",
+			}, nil
+		},
+		constructionZonePath: "/my/cons/path",
+		versionDeps: func(args verdeps.VersionDepsArgs) error {
+			assert.Equal(t, "myauthor", args.Author)
+			assert.Equal(t, "myrepo", args.Repo)
+			assert.Equal(t, "mysha", args.SHA)
+			assert.Equal(t, "/archive/dir/path", args.Path)
+			return nil
+		},
+		attemptWorkDirDeletion: func(workDirPath string) {
+			return
+		},
+		createDepotRepo: func(author, repo, sha string) (bool, error) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return true, nil
+		},
+		pushToDepot: func(args packagePusherArgs) error {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return nil
+		},
+		recordPackageArchival: func(args packageArchivalArgs) {
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			return
+		},
+	}
+	err = versionAndArchivePackage(args)
+	assert.Nil(t, err)
 }
