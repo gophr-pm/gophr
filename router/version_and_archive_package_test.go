5fbf1aca52b59a0b607c4400e552c425cc5e9040
Sandile Keswa <me@sandile.io>
2016-09-20 00:33:25 -0400

    create second test

--- router/version_and_archive_package_test.go
+++ router/version_and_archive_package_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/skeswa/gophr/common/verdeps"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,5 +25,33 @@ func TestVersionAndArchivePackage(t *testing.T) {
 		constructionZonePath: "/my/cons/path",
 	}
 	err := versionAndArchivePackage(args)
+	assert.NotNil(t, err, "this should return an error")
+
+	args = packageVersionerArgs{
+		sha:    "mysha",
+		repo:   "myrepo",
+		author: "myauthor",
+		downloadPackage: func(args packageDownloaderArgs) (packageDownloadPaths, error) {
+			// TODO(skeswa): @shikkic, write something that tests if "args" is correct
+			// here. :D kthxbai
+			assert.Equal(t, "myauthor", args.author)
+			assert.Equal(t, "myrepo", args.repo)
+			assert.Equal(t, "mysha", args.sha)
+			assert.Equal(t, "/my/cons/path", args.constructionZonePath)
+			return packageDownloadPaths{
+				archiveDirPath: "/archive/dir/path",
+			}, nil
+		},
+		constructionZonePath: "/my/cons/path",
+		versionDeps: func(args verdeps.VersionDepsArgs) error {
+			assert.Equal(t, "myauthor", args.Author)
+			assert.Equal(t, "myrepo", args.Repo)
+			assert.Equal(t, "mysha", args.SHA)
+			assert.Equal(t, "/archive/dir/path", args.Path)
+			return errors.New("this is an error")
+		},
+	}
+	err = versionAndArchivePackage(args)
 	assert.NotNil(t, err)
+
 }
