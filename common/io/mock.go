5d74b35f690886c67cb4df1c8dd2471ea244e611
Sandile Keswa <me@sandile.io>
2016-09-20 22:11:09 -0400

    updated the io mock

--- common/io/mock.go
+++ common/io/mock.go
@@ -1,6 +1,7 @@
 package io
 
 import (
+	"io"
 	"os"
 
 	"github.com/stretchr/testify/mock"
@@ -11,11 +12,28 @@ type MockIO struct {
 	mock.Mock
 }
 
-// Mkdir creates a new directory with the specified name and permission bits.
-// If there is an error, it will be of type *PathError.
-func (io *MockIO) Mkdir(name string, perm os.FileMode) error {
-	args := io.Called(name, perm)
+// Mkdir calls os.Mkdir.
+func (m *MockIO) Mkdir(name string, perm os.FileMode) error {
+	args := m.Called(name, perm)
 	return args.Error(0)
+}
+
+// Create calls os.Create.
+func (m *MockIO) Create(name string) (*os.File, error) {
+	args := m.Called(name)
+	return args.Get(0).(*os.File), args.Error(1)
+}
+
+// Copy calls io.Copy.
+func (m *MockIO) Copy(dst io.Writer, src io.Reader) (int64, error) {
+	args := m.Called(dst, src)
+	return args.Get(0).(int64), args.Error(1)
+}
+
+// ReadDir calls ioutil.ReadDir.
+func (m *MockIO) ReadDir(dirname string) ([]os.FileInfo, error) {
+	args := m.Called(dirname)
+	return args.Get(0).([]os.FileInfo), args.Error(1)
 }
 
 // NewMockIO creates a new IO mock.
