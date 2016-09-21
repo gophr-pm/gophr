50a74c38c4b4c02e6011751dc2e54487ae1a96bf
Sandile Keswa <me@sandile.io>
2016-09-21 01:11:55 -0400

    tests pass so far - about half way through

--- router/download_package_test.go
+++ router/download_package_test.go
@@ -120,4 +120,42 @@ func TestDownloadPackage(t *testing.T) {
 	mockIO.AssertExpectations(t)
 	assert.True(t, zipResp.Body.(*common.MockHTTPResponseBody).WasClosed())
 	assert.True(t, deleteWorkDirCalled)
+
+	// TODO(skeswa): come up with a mock file to make sure it is closed.
+	mockFile := &os.File{}
+	zipResp = &http.Response{
+		StatusCode: 200,
+		Body:       common.NewMockHTTPResponseBody([]byte("this is a zip")),
+	}
+	mockIO = io.NewMockIO()
+	mockIO.
+		On("Mkdir", mock.AnythingOfType("string"), os.FileMode(0644)).
+		Return(nil)
+	mockIO.
+		On("Create", mock.AnythingOfType("string")).
+		Return(mockFile, error(nil))
+	mockIO.
+		On("Copy", mockFile, zipResp.Body).
+		Return(int64(0), errors.New("the copy didnt work"))
+	deleteWorkDirCalled = false
+	args = packageDownloaderArgs{
+		io:                   mockIO,
+		author:               "myauthor",
+		repo:                 "myrepo",
+		sha:                  "mysha",
+		constructionZonePath: "/my/cons/zone",
+
+		doHTTPGet: func(url string) (*http.Response, error) {
+			assert.Equal(t, "https://github.com/myauthor/myrepo/archive/mysha.zip", url)
+			return zipResp, nil
+		},
+		deleteWorkDir: func(folderPath string) {
+			deleteWorkDirCalled = true
+		},
+	}
+	_, err = downloadPackage(args)
+	assert.NotNil(t, err)
+	mockIO.AssertExpectations(t)
+	assert.True(t, zipResp.Body.(*common.MockHTTPResponseBody).WasClosed())
+	assert.True(t, deleteWorkDirCalled)
 }
