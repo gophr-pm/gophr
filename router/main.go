ade852586866af3c27c50443e9033bdeb036d370
Shikkic <Shikkic@gmail.com>
2016-09-29 23:45:28 -0400

    Track transaction in router with new relic

--- router/main.go
+++ router/main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/gophr-pm/gophr/common"
 	"github.com/gophr-pm/gophr/common/config"
+	"github.com/gophr-pm/gophr/common/newrelic"
+	newrelic "github.com/newrelic/go-agent"
 )
 
 func main() {
@@ -17,11 +19,24 @@ func main() {
 		log.Fatalln("Failed to read credentials secret:", err)
 	}
 
+	var app newrelic.Application
+	if !conf.IsDev {
+		newRelicKey, err := nr.GenerateKey(conf)
+		if err != nil {
+			log.Fatalln("Failed to read newrelic credentials secret:", err)
+		}
+		config := newrelic.NewConfig("Gophr", newRelicKey)
+		app, err = newrelic.NewApplication(config)
+		if err != nil {
+			log.Fatalln("Failed to create new relic monitoring application:", err)
+		}
+	}
+
 	// Ensure that the session is closed eventually.
 	defer db.Close()
 
 	// Start serving.
-	http.HandleFunc(wildcardHandlerPattern, RequestHandler(conf, db, creds))
+	http.HandleFunc(wildcardHandlerPattern, RequestHandler(conf, db, creds, app))
 	log.Printf("Servicing HTTP requests on port %d.\n", conf.Port)
 	http.ListenAndServe(fmt.Sprintf(":%d", conf.Port), nil)
 }
--- router/request_handler.go
+++ router/request_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gocql/gocql"
@@ -9,6 +10,7 @@ import (
 	"github.com/gophr-pm/gophr/common/errors"
 	"github.com/gophr-pm/gophr/common/github"
 	"github.com/gophr-pm/gophr/common/io"
+	"github.com/newrelic/go-agent"
 )
 
 const (
@@ -25,7 +27,9 @@ var (
 func RequestHandler(
 	conf *config.Config,
 	session *gocql.Session,
-	creds *config.Credentials) func(http.ResponseWriter, *http.Request) {
+	creds *config.Credentials,
+	newRelicApp newrelic.Application,
+) func(http.ResponseWriter, *http.Request) {
 	// Instantiate the IO module for use in package downloading and versioning.
 	io := io.NewIO()
 
@@ -38,6 +42,13 @@ func RequestHandler(
 	})
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Create a new relic transaction.
+		if !conf.IsDev {
+			log.Printf("Logging request for %s \n", r.URL.String())
+			txn := newRelicApp.StartTransaction(r.URL.String(), w, r)
+			defer txn.End()
+		}
+
 		// Make sure that this isn't a simple health check before getting more
 		// complicated.
 		if r.URL.Path == healthCheckRoute {
