dbbe62b7c1b0b2661a8b15538432cfc9a831c608
Zachary.smith <Zachary.smith@yodle.com>
2016-10-07 00:39:22 -0400

    prove build breaks with failing test

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+EXIT_STATUS=0
+
+echo "mode: count" > coverage-all.out
+for pkg in $( find . -name *.go -print0 | xargs -0 -n1 dirname | sort --unique ); do
+    go test -coverprofile=coverage.out -covermode=count $pkg;
+    if [ $? -ne 0 ]; then EXIT_STATUS=1; fi;
+    tail -n +2 coverage.out >> coverage-all.out;
+done
+echo "exiting with status $EXIT_STATUS";
+exit $EXIT_STATUS;
