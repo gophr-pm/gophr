2a9af4c2e28a8a505723e1ae7bf4b7b4ae252580
Shikkic <Shikkic@gmail.com>
2016-10-14 15:46:06 -0400

    rename indexer and create new main for new scheduler

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,69 +1,58 @@
 package main
 
 import (
-	"log"
-	"sync"
+	"fmt"
+	"net/http"
 
-	"github.com/gophr-pm/gophr/common"
-	"github.com/gophr-pm/gophr/common/models"
+	"github.com/robfig/cron"
 )
 
 func main() {
-	log.Println("Preparing to initialize DB connection")
-	conf, session := common.Init()
-	defer session.Close()
+	c := cron.New()
 
-	log.Println("Fetching godoc metadata")
-	godocMetadataList, err := fetchGodocMetadata()
-	if err != nil {
-		log.Println("Failed to fetch godoc metadata")
-		log.Fatalln(err)
+	c.AddFunc("* * * * * *", func() { fmt.Println("Hey") })
+	c.AddFunc("* * * * * *", Github_indexer)
+
+	// Start the cron service
+	c.Start()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Requ	est) { fmt.Println("LOL") })
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func scanPackageModels(query *gocql.Query) ([]*PackageModel, error) {
+	var (
+		err          error
+		scanError    error
+		closeError   error
+		packageModel *PackageModel
+
+		repo        string
+		author      string
+		description string
+
+		iter          = query.Iter()
+		packageModels = make([]*PackageModel, 0)
+	)
+
+	for iter.Scan(&repo, &author, &description) {
+		packageModel, err = NewPackageModelFromBulkSelect(author, repo, description)
+		if err != nil {
+			scanError = err
+			break
+		} else {
+			packageModels = append(packageModels, packageModel)
+		}
 	}
 
-	log.Println("Fetching awesome go list")
-	awesomeGoIndex, err := fetchAwesomeGoList()
-	if err != nil {
-		log.Println("Failed to fetch awesome go list")
-		log.Fatalln(err)
+	if err = iter.Close(); err != nil {
+		closeError = err
 	}
 
-	log.Println("Preparing to build package models")
-	packageModels, err := buildPackageModels(godocMetadataList, awesomeGoIndex)
-	if err != nil {
-		log.Println("Failed to build package models")
-		log.Fatalln(err)
+	if scanError != nil || closeError != nil {
+		return nil, errors.NewQueryScanError(scanError, closeError)
 	}
 
-	log.Println("Preparing to insert packages into database")
-
-	var wg sync.WaitGroup
-	var insertPackageErrors []error
-
-	nbConcurrentInserts := 20
-	packageChan := make(chan *models.PackageModel, nbConcurrentInserts)
-	for i := 0; i < nbConcurrentInserts; i++ {
-		wg.Add(1)
-		go func() {
-			for packageModel := range packageChan {
-				err := models.InsertPackage(session, packageModel)
-				if err != nil {
-					insertPackageErrors = append(insertPackageErrors, err)
-				}
-			}
-			wg.Done()
-		}()
-	}
-
-	for _, packageModel := range packageModels {
-		packageChan <- packageModel
-	}
-	close(packageChan)
-	wg.Wait()
-
-	for _, insertErr := range insertPackageErrors {
-		log.Println(insertErr)
-	}
-
-	log.Println("Finished inserting packages into database")
-	ReIndexPackageGitHubStats(conf, session)
+	return packageModels, nil
 }
