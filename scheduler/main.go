553f9f2d707c92984090b35a08417fcfbf5f1100
Shikkic <Shikkic@gmail.com>
2016-10-16 17:40:57 -0400

    re-write base scheduler

--- scheduler/main.go
+++ scheduler/main.go
@@ -8,51 +8,23 @@ import (
 )
 
 func main() {
+	// Instantiate new cron instance.
 	c := cron.New()
 
-	c.AddFunc("* * * * * *", func() { fmt.Println("Hey") })
+	// List cron jobs.
 	c.AddFunc("* * * * * *", Github_indexer)
 
-	// Start the cron service
+	// Start the cron process.
 	c.Start()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Requ	est) { fmt.Println("LOL") })
+	// Create handlers for generating metrics.
+	http.HandleFunc("/daily-downloads", dailyDownloadsHandler)
+
+	// Start HTTP server.
 	http.ListenAndServe(":8080", nil)
 }
 
-
-func scanPackageModels(query *gocql.Query) ([]*PackageModel, error) {
-	var (
-		err          error
-		scanError    error
-		closeError   error
-		packageModel *PackageModel
-
-		repo        string
-		author      string
-		description string
-
-		iter          = query.Iter()
-		packageModels = make([]*PackageModel, 0)
-	)
-
-	for iter.Scan(&repo, &author, &description) {
-		packageModel, err = NewPackageModelFromBulkSelect(author, repo, description)
-		if err != nil {
-			scanError = err
-			break
-		} else {
-			packageModels = append(packageModels, packageModel)
-		}
-	}
-
-	if err = iter.Close(); err != nil {
-		closeError = err
-	}
-
-	if scanError != nil || closeError != nil {
-		return nil, errors.NewQueryScanError(scanError, closeError)
-	}
-
-	return packageModels, nil
+// dailyDownloadsHandler responsible running a metric job via a handlr.
+func dailyDownloadsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("placeholder")
 }
