5654d68b3c8a497fd3d2e74719981487c76fb394
Sandile Keswa <me@sandile.io>
2016-10-23 04:08:50 -0400

    started on traverse package dir test

---
+++ lib/verdeps/traverse_package_dir_test.go
@@ -0,0 +1,12 @@
+package verdeps
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTraversePackageDir(t *testing.T) {
+	Convey("Given a package dir to traverse", t, func() {
+	})
+}
