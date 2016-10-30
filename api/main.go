d9a6f25e56b02438b15bbc733b91633ea1132086
Sandile Keswa <me@sandile.io>
2016-10-30 01:08:28 -0400

    got rid of the test route in the api

--- api/main.go
+++ api/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gophr-pm/gophr/lib"
-	"github.com/gophr-pm/gophr/lib/db/model/package/download"
 	"github.com/gorilla/mux"
 )
 
@@ -47,40 +45,6 @@ func main() {
 		urlVarAuthor,
 		urlVarRepo),
 		GetPackageHandler(client)).Methods("GET")
-
-	// TODO(skeswa): delete this please.
-	r.HandleFunc(fmt.Sprintf(
-		"/downloads/{%s}/{%s}",
-		urlVarAuthor,
-		urlVarRepo),
-		func(w http.ResponseWriter, r *http.Request) {
-			vars := mux.Vars(r)
-			author := vars[urlVarAuthor]
-			repo := vars[urlVarRepo]
-
-			fmt.Printf("got /downloads/%s/%s\n", author, repo)
-			splits, err := download.GetSplits(client, author, repo)
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte("could not get downloads: " + err.Error()))
-				return
-			}
-
-			splitsMap := map[string]int{
-				"daily":   splits.Daily,
-				"weekly":  splits.Weekly,
-				"monthly": splits.Monthly,
-				"allTime": splits.AllTime,
-			}
-			splitsJSON, err := json.Marshal(splitsMap)
-			if err != nil {
-				w.WriteHeader(500)
-				w.Write([]byte("could not marshal json: " + err.Error()))
-				return
-			}
-
-			respondWithJSON(w, splitsJSON)
-		}).Methods("GET")
 
 	// Start serving.
 	log.Printf("Servicing HTTP requests on port %d.\n", config.Port)
