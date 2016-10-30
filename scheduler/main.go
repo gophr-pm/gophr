899cdfe1b679f782359d9239b5714663e3eee5e8
Shikkic <Shikkic@gmail.com>
2016-10-30 19:24:21 -0400

    update scheduler main

--- scheduler/main.go
+++ scheduler/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gophr-pm/gophr/lib"
+	"github.com/gophr-pm/gophr/lib/github"
+	"github.com/gophr-pm/gophr/lib/model"
 	"github.com/gophr-pm/gophr/scheduler/indexer/awesome"
 	"github.com/gophr-pm/gophr/scheduler/indexer/github"
 	"github.com/gophr-pm/gophr/scheduler/indexer/godoc"
@@ -31,7 +34,20 @@ func main() {
 	})
 
 	c.AddFunc("0 0 * * * *", godoc.Index)
-	c.AddFunc("0 0 0 * * *", github.Index)
+
+	c.AddFunc("0 0 0 * * *", func() {
+		if err := githubIndexer.Index(githubIndexer.IndexArgs{
+			Init:                    common.Init,
+			PackageDeleter:          models.DeletePackageModel,
+			PackageRetriever:        models.ScanAllPackageModels,
+			PackageInserter:         models.InsertPackage,
+			NewGithubRequestService: github.NewRequestService,
+			RequestTimeBuffer:       50 * time.Millisecond,
+		}); err != nil {
+			// TODO(Shikkic): Send error somewhere, possibly deadman's snitch?
+			log.Println(err)
+		}
+	})
 
 	// Start the cron process.
 	c.Start()
