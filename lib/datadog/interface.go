90a5221677f521590788b53204aeca9420fc5c23
Shikkic <Shikkic@gmail.com>
2016-11-12 19:24:53 -0500

    added comments

--- lib/datadog/interface.go
+++ lib/datadog/interface.go
@@ -6,16 +6,20 @@ import (
 	"github.com/DataDog/datadog-go/statsd"
 )
 
+// Constants used to references against statsd's unexported
+// AlertType.
 const (
 	Error   = "error"
 	Info    = "info"
 	Success = "success"
 )
 
-// EventCreator lol
+// EventCreator is a interface for passing a statsd DataDog NewEvent
+// and responsible - Google Search for creating new metric events.
 type EventCreator func(title, text string) *statsd.Event
 
-// TrackTranscationArgs lol
+// TrackTranscationArgs is the args structs for tracking transactions
+// to DataDog.
 type TrackTranscationArgs struct {
 	Tags            []string
 	MetricName      string
